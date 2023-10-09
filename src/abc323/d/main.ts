a67bdacd2c3a378026f4bd2b6574229d7044571a
itaosan <itaosan3@gmail.com>
2023-10-09 14:38:15 +0900

    abc322-d,e 解説AC

--- src/abc323/d/main.ts
+++ src/abc323/d/main.ts
@@ -1,22 +1,32 @@
 import * as readline from "readline";
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+type Pair = {
+  x: number;
+  y: bigint;
+};
+
 let inputCount = 0;
 let n = 0;
-const mp: Map<bigint, bigint> = new Map();
+const mp: Map<number, bigint> = new Map();
 
 rl.on("line", (input: string) => {
   if (inputCount === 0) {
     n = parseInt(input);
     inputCount++;
   } else {
-    const [x, y] = input.split(" ").map(BigInt);
-    //各スライムの個数にmap
-    //存在する場合は足しこむ
+    const inputs = input.split(" ").map((s) => parseInt(s));
+    let x = inputs[0];
+    let y = BigInt(inputs[1]);
+
+    while ((x & 1) === 0) {
+      x >>= 1;
+      y <<= 1n;
+    }
+
     mp.set(x, (mp.get(x) || 0n) + y);
 
     if (inputCount === n) {
@@ -30,17 +40,10 @@ rl.on("line", (input: string) => {
 rl.on("close", () => {
   let ans = 0;
 
-  for (const [x, y] of mp.entries()) {
-    if (y > 1n) {
-      //2個以上なら合成
-      const newX = x * 2n;
-      //合成語のサイズの Mapに足しこむ
-      //入力例が昇順なので小さい方に足しこまれることはない
-      mp.set(newX, (mp.get(newX) || 0n) + y / 2n);
-    }
-    if (y & 1n) {
-      //奇数の場合は合成できない、匹を足す
-      ans++;
+  for (let [_, y] of mp.entries()) {
+    while (y > 0n) {
+      if (y & 1n) ans++;
+      y >>= 1n;
     }
   }
 
