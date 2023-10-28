f2c49b087f995c2858286772baefe9826fed8683
itaosan <itaosan3@gmail.com>
2023-10-28 20:56:22 +0900

    abc325

---
+++ src/abc325/a/main.ts
@@ -0,0 +1,18 @@
+import * as readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input: string[] = [];
+
+rl.on("line", (line) => {
+  input.push(line);
+});
+
+rl.on("close", () => {
+  //const N = parseInt(input[0]);
+  const S = input[0].split(" ").map(String);
+  console.log(S[0] + " san")
+});
