fbc2b9fdbecca9618bc191c1436b4c8fe4bd2171
itaosan <itaosan3@gmail.com>
2024-04-13 23:14:20 +0900

    abc349

---
+++ src/abc349/a/main.ts
@@ -0,0 +1,19 @@
+import * as fs from "node:fs";
+
+//移動する方向
+const dx = [0, 1, 0, -1];
+const dy = [-1, 0, 1, 0];
+
+const input = fs.readFileSync("/dev/stdin", "utf8").split("\n");
+let inputIndex = 0;
+const readLine = () => input[inputIndex++];
+
+const N = Number.parseInt(readLine());
+const A = readLine().split(" ").map(Number);
+//const S = readLine()
+
+let sum =0
+for (let i = 0; i < N-1; i++) {
+  sum -= A[i]
+}
+console.log(sum)
