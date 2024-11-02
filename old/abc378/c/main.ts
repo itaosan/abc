45fe01de46376bc8ba3efa24a5f24df1209eacd4
itaosan <itaosan3@gmail.com>
2024-11-02 22:51:23 +0900

    abc378

---
+++ old/abc378/c/main.ts
@@ -0,0 +1,50 @@
+import * as fs from "node:fs";
+
+//移動する方向
+//const dx = [0, 1, 0, -1];
+//const dy = [-1, 0, 1, 0];
+
+// 頂点数 N を前提として、隣接リスト G を作成
+//const G: number[][] = Array.from({ length: N + 1 }, () => []);
+
+// 自作のキュー
+class Queue<T> {
+  private data: T[] = [];
+  private front = 0;
+
+  push(item: T): void {
+    this.data.push(item);
+  }
+
+  pop(): T | undefined {
+    if (this.front >= this.data.length) return undefined;
+    const item = this.data[this.front];
+    this.front++;
+    return item;
+  }
+
+  isEmpty(): boolean {
+    return this.front >= this.data.length;
+  }
+}
+
+const input = fs.readFileSync("/dev/stdin", "utf8").split("\n");
+let inputIndex = 0;
+const readLine = () => input[inputIndex++];
+
+const N = Number.parseInt(readLine());
+const A = readLine().split(" ").map(Number);
+//const S = readLine()
+const work = new Map<number, number>();
+const ans = [];
+
+for (let i = 0; i < N; i++) {
+  if (!work.has(A[i])) {
+    ans.push(-1);
+  } else {
+    ans.push(work.get(A[i]));
+  }
+  work.set(A[i], i + 1)
+}
+
+console.log(ans.join(" "));
