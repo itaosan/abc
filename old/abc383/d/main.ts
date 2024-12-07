95b86d813a18f7d759dc29c5c560242cd2dd226a
itaosan <itaosan3@gmail.com>
2024-12-07 23:20:54 +0900

    abc383

---
+++ old/abc383/d/main.ts
@@ -0,0 +1,8 @@
+import * as fs from "node:fs";
+
+const input = fs.readFileSync("/dev/stdin", "utf8").split("\n");
+let inputIndex = 0;
+const readLine = () => input[inputIndex++];
+
+const N = Number.parseInt(readLine());
+
